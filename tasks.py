670a521861e2c907f249c82f7697a93d8ef35d01
dbetm <davbetm@gmail.com>
2023-05-26 19:32:05 -0600

    separate requirements by experiment

--- tasks.py
+++ tasks.py
@@ -8,3 +8,11 @@ $ invoke run-unit-tests
 @task
 def tests(c):
     c.run("python3 -b -m pytest -s -l -v --rootdir=tests/")
+
+
+@task
+def setup_experiment(c, exp: int):
+    """Example: `invoke setup-experiment --exp 0`"""
+    c.run(f"pip install --upgrade pip")
+    c.run(f"pip install -r requirements-exp.txt")
+    c.run(f"pip install -r experiments/exp_{exp}/requirements.txt")
