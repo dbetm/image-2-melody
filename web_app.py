5b3e23e1a63dd6d39b7fe38379a5bb8a4d8c3bfe
dbetm <davbetm@gmail.com>
2023-05-27 08:14:12 -0600

    add exp 3 to web app

--- web_app.py
+++ web_app.py
@@ -1,16 +1,17 @@
 import logging as logger
 from datetime import datetime
-from os import environ
+from os import environ, path, remove
 
 import streamlit as st
 
-from experiments.exp_2 import exp_2
-from run_exp import run_exp
+from run_exp import load_module, run_exp
 
 
 logger.basicConfig(level=logger.INFO)
 
 IMAGE_KEY = "image"
+EXP_ID_KEY = "expID"
+ALLOWED_EXP_IDS = [2, 3]
 
 
 def save(uploaded_file, path: str):
@@ -19,12 +20,38 @@ def save(uploaded_file, path: str):
 
 
 def reset():
-    del st.session_state[IMAGE_KEY]
+    if st.session_state.get(IMAGE_KEY):
+        del st.session_state[IMAGE_KEY]
+
+
+def remove_tmp_files(filepaths: list):
+    for filepath in filepaths:
+        try:
+            remove(filepath)
+            logger.info(f"Deleted file: {filepath}")
+        except OSError as e:
+            logger.warning(f"Error deleting file: {filepath} - {e}")
+
+
+def resolve_exp(exp_id: int) -> tuple:
+    assert exp_id in ALLOWED_EXP_IDS, "Select an appropiate experiment id (3 or 5)"
+
+    exp_module = load_module(exp_id)
+    exp_info = exp_module.__doc__
+
+    return (exp_module, exp_info)
 
 
 def run():
     st.title("Image to Melody")
     st.write("Upload an image and we'll convert it into a short musical video.")
+
+    selected_exp_id = st.selectbox(
+        "Choose an experiment", ALLOWED_EXP_IDS, on_change=reset
+    )
+    exp_module, exp_info = resolve_exp(selected_exp_id)
+
+    st.write("Experiment info:", exp_info)
 
     uploaded_file = st.file_uploader(
         "Choose an image file", type=["jpg", "jpeg", "png"], key=IMAGE_KEY
@@ -32,27 +59,33 @@ def run():
 
     if uploaded_file is not None:
         image_filename = uploaded_file.name
+        image_filepath = path.join("sample_images", image_filename)
 
-        save(uploaded_file, path=f"sample_images/{image_filename}")
+        save(uploaded_file, path=image_filepath)
 
         st.image(uploaded_file, width=300)
 
         logger.info({"image": image_filename})
 
-        with st.spinner(f"Running experiment 2 for image: {image_filename}"):
+        with st.spinner(f"Running experiment {selected_exp_id} for image: {image_filename}"):
             video_filepath = run_exp(
-                exp=exp_2, image_filenames=[image_filename], create_video=True
+                exp=exp_module, image_filenames=[image_filename], create_video=True
             )
 
         st.success("Video generated successfully!")
         st.write("Download your video:")
 
-        now_str = datetime.now().strftime("%Y%m%d_%H%m")
+        now_str = datetime.now().strftime("%Y%m%d_%H%M")
         filename = f"{now_str}_image_to_melody.mp4"
 
         with open(video_filepath, "rb") as f:
             logger.info({"video": filename})
             st.download_button("Download", f, file_name=filename, on_click=reset)
+
+        audio_filepath = video_filepath.replace(
+            "final_audio.mp4", image_filename.split(".")[0] + ".wav"
+        )
+        remove_tmp_files([video_filepath, image_filepath, audio_filepath])
 
 
 
